92ece596da26d9216c05662ce3fa4e01c4f719f9
fsantsil <felipetssilva@hotmail.com>
2025-02-19 13:18:29 +0100

    update: update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.87.0"
+      region = "us-east-1"
+
+    }
+  }
+  backend "s3" {
+    bucket = "s3-backend-state"
+    key    = "s3-backup"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
